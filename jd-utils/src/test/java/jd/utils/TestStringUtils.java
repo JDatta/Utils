147c119c3077452a05ecd6de2eb7a4e63e3b51d3
Joydip Datta <jdatta@talena-inc.com>
2015-12-24 23:52:56 +0530

    Add few basic string utils

---
+++ jd-utils/src/test/java/jd/utils/TestStringUtils.java
@@ -0,0 +1,28 @@
+package jd.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestStringUtils {
+
+  private String str =
+    "\n\nCREATE TABLE jd.table1\n(\n    "
+      + "\"timestamp\" timestamp,\n    table1 int,\n    "
+      + "f_cookie varchar(2048),\n    ipaddr varchar(2048),\n    "
+      + "DEFAULT regexp_replace(table1.f_cookie, 'abc;abc', '')\n);\n\n"
+      + "ALTER TABLE jd.table1 ADD PRIMARY KEY (f_cookie)";
+
+  @Test
+  public void testQuotedIndexOf() {
+    Assert.assertEquals(184, StringUtils.indexOf(str, ';', '\''));
+  }
+
+  @Test
+  public void testSplitOnce() {
+    String expected = "\n\nCREATE TABLE jd.table1\n(\n    "
+      + "\"timestamp\" timestamp,\n    table1 int,\n    "
+      + "f_cookie varchar(2048),\n    ipaddr varchar(2048),\n    "
+      + "DEFAULT regexp_replace(table1.f_cookie, 'abc;abc', '')\n)";
+    Assert.assertEquals(expected, StringUtils.splitOnce(str, ';', '\''));
+  }
+}
